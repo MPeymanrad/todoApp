99dbebed1abf18cc89b8bccc532255c2bee87875
Peymanrad <peymanradmo@gmail.com>
2022-05-13 12:24:09 +0430

    Selected DOM elementes in JS script

---
+++ app.js
@@ -0,0 +1,9 @@
+const $ = document;
+const addBtn = $.querySelector('.add_btn')
+const addModal = $.querySelector('.add_modal')
+const addModalOverlay = $.querySelector('.overlay')
+//modal elements
+const todoTitleInput = $.getElementById('todo_title_input')
+const todoDescriptionInput = $.getElementById('todo_description_input')
+const modalCloseBtn = $.querySelector('.close_btn')
+const todoSubmitBtn = $.querySelector('.add_todo_btn')
