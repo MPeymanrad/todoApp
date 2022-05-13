ecede75e9fe2649d3c07748af3ccd969bac38a52
Peymanrad <peymanradmo@gmail.com>
2022-05-13 12:27:55 +0430

    Implemented hide and show functionalities for modal

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ const todoTitleInput = $.getElementById('todo_title_input')
 const todoDescriptionInput = $.getElementById('todo_description_input')
 const modalCloseBtn = $.querySelector('.close_btn')
 const todoSubmitBtn = $.querySelector('.add_todo_btn')
+
+function showModal() {
+    addModal.style.top = '20%'
+    addModalOverlay.style.display = 'block'
+}
+function hideModal() {
+    addModal.style.top = '-100%'
+    addModalOverlay.style.display = 'none'
+}
+
+addBtn.addEventListener('click',showModal)
+modalCloseBtn.addEventListener('click',hideModal)
