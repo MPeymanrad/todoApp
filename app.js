67e1192650edc1088c75d7624bfe97a592a21a16
Peymanrad <peymanradmo@gmail.com>
2022-05-14 12:41:19 +0430

    Added basic feature of complete,edit,delete todos

--- app.js
+++ app.js
@@ -22,17 +22,38 @@ function createTodoElem(title) {
   const todo = $.createElement("div");
   todo.classList.add("todo");
   const todoInners = `<h3 class="todo_title">${title}</h3><div class="todo_actions"><span class="do_todo material-symbols-outlined" title="Complete Todo">done</span><span class="edit_todo material-symbols-outlined" title="Edit Todo">edit</span><span class=" delete_todo material-symbols-outlined" title="Delete Todo">delete</span></div>`;
-
   todo.innerHTML = todoInners;
   todosContainer.append(todo);
 }
+function completeTodo(e) {
+  e.target.parentElement.parentElement.style.textDecorationLine =
+    "line-through";
+}
+function editTodo() {
+  //get data from local storage...
+
+  showModal();
+}
+function deleteTodo(e) {
+  e.target.parentElement.parentElement.remove();
+}
 function setEventsForTodoActions() {
-  let doBtns = $.querySelectorAll(".do_todo");
-  let editBtns = $.querySelectorAll(".edit_todo");
-  let delBtns = $.querySelectorAll(".delete_todo");
+  const doTodoElems = $.querySelectorAll(".do_todo");
+  const editTodoElems = $.querySelectorAll(".edit_todo");
+  const delTodoElems = $.querySelectorAll(".delete_todo");
+  doTodoElems.forEach(function (item) {
+    item.addEventListener("click", completeTodo);
+  });
+  editTodoElems.forEach(function (item) {
+    item.addEventListener("click", editTodo);
+  });
+  delTodoElems.forEach(function (item) {
+    item.addEventListener("click", deleteTodo);
+  });
 }
 function addTodo() {
   createTodoElem(todoTitleInput.value);
+  setEventsForTodoActions();
   hideModal();
 }
 
